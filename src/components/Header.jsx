b7313e96ccd3ce791070023335f3939538221960
Israel Akinmoju <kolawoleisrael546@gmail.com>
2025-08-24 16:29:31 +0100

    navigation and landing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import "../App.css"
+import morsk from "../assets/morsk.png"
 
 const Header = () => {
 		return (
-				<div>Header</div>
-		)
+			<header>
+				<nav>
+					<div className="image"><img src={morsk} alt="morsk logo" width="125px" /></div>
+					<ul>
+						<li><a href="#">Why Morsk?</a></li>
+						<li><a href="#">About us</a></li>
+						<li><a href="#">Solutions</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+					<button>Hire us</button>
+					</nav>
+					<div className="main"></div>
+			</header>
+						)
 }
 
 export default Header
---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../App.css"
+
+const Main = () => {
+		return (
+			<main>
+				<section className='introduction' >
+					<h1>Got some tech needs? <br />don't fret <br />let's solve them for you</h1>
+					<h3>same place, all in one solutions</h3>
+					<h4>Send an email to get started</h4>
+				</section>
+				<section className='why-morsk'></section>
+			</main>
+
+		)
+}
+// https://www.youtube.com/playlist?list=PLpwngcHZlPae68z_mLFNfbJFIJVJ_Zcx2
+export default Main
