7b97a6e09a9058f2e6b4b73bcf7286cacd1c3ab9
Israel Akinmoju <kolawoleisrael546@gmail.com>
2025-09-01 11:36:27 +0100

    more styling adjustments for full page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,37 +3,31 @@ import "../styles/projects.css";
 
 const Projects = () => {
   return (
-    <section className="projects" >
+    <section className="projects">
       <p style={{ fontSize: "2rem" }}> Explore our Projects</p>
       <section className="graphics-design">
         <br />
         <p>Flyers</p>
         <br />
         <div className="flyers-container">
-          <img src="/real int ad.jpg" alt="" />
-          <img src="/real max ad.jpg" alt="" />
-          <img src="/freight bunny.jpg" alt="" />
-          <img src="/gloryland.jpg" alt="" />
-
-          <img src="/gdg.jpg" alt="" />
-
-          <img src="/lacadous.jpg" alt="" />
-
-          <img src="/morsk.jpg" alt="" />
-
-          <img src="/shes-deemua.jpg" alt="" />
-
-          <img src="/kindred spirits.jpg" alt="" />
-
-          <img src="/neephia stitches.jpg" alt="" />
+          <img src="/real int ad.jpg" alt="" loading="lazy" />
+          <img src="/real max ad.jpg" alt="" loading="lazy" />
+          <img src="/freight bunny.jpg" alt="" loading="lazy" />
+          <img src="/gloryland.jpg" alt="" loading="lazy" />
+          <img src="/gdg.jpg" alt="" loading="lazy" />
+          <img src="/lacadous.jpg" alt="" loading="lazy" />
+          <img src="/morsk.jpg" alt="" loading="lazy" />
+          <img src="/shes-deemua.jpg" alt="" loading="lazy" />
+          <img src="/kindred spirits.jpg" alt="" loading="lazy" />
+          <img src="/neephia stitches.jpg" alt="" loading="lazy" />
         </div>
         <br /> <br />
         <p>Social Media Posts</p>
         <br />
         <div className="social-media-container">
-          <img src="/nikee.jpg" alt="" />
-          <img src="/monsta or food.jpg" alt="" />
-          <img src="/robot.jpg" alt="" />
+          <img src="/nikee.jpg" alt="" loading="lazy" />
+          <img src="/monsta or food 2.jpg" alt="" loading="lazy" />
+          <img src="/robot.jpg" alt="" loading="lazy" />
         </div>
         <br />
       </section>
@@ -44,15 +38,15 @@ const Projects = () => {
         <p>Logo Designs</p>
         <br />
         <div className="logos-container">
-          <img src="/eco-flare.jpg" alt="" />
-          <img src="/anhor-point.jpg" alt="" />
-          <img src="/beets.jpg" alt="" />
-          <img src="/fitclad.jpg" alt="" />
-          <img src="/illuminiq.jpg" alt="" />
-          <img src="/konnect-circuit.jpg" alt="" />
-          <img src="/next-pay.jpg" alt="" />
-          <img src="/thunder-forte.jpg" alt="" />
-          <img src="/uplink.jpg" alt="" />
+          <img src="/eco-flare.jpg" alt="" loading="lazy" />
+          <img src="/anhor-point.jpg" alt="" loading="lazy" />
+          <img src="/beets.jpg" alt="" loading="lazy" />
+          <img src="/fitclad.jpg" alt="" loading="lazy" />
+          <img src="/illuminiq.jpg" alt="" loading="lazy" />
+          <img src="/konnect-circuit.jpg" alt="" loading="lazy" />
+          <img src="/next-pay.jpg" alt="" loading="lazy" />
+          <img src="/thunder-forte.jpg" alt="" loading="lazy" />
+          <img src="/uplink.jpg" alt="" loading="lazy" />
         </div>
         <br />
       </section>
@@ -63,7 +57,7 @@ const Projects = () => {
         <p>Brand Designs</p>
         <br />
         <div className="brands-container">
-          <img src="/real int ad.jpg" alt="" />
+          <img src="/real int ad.jpg" alt="" loading="lazy" />
         </div>
         <br />
       </section>
@@ -75,7 +69,7 @@ const Projects = () => {
         <br />
         <div className="websites-container">
           <div className="website">
-            <img src="/solutions.jpg" alt="" />
+            <img src="/solutions.jpg" alt="" loading="lazy" />
             <p>Tenzies Game</p>
           </div>
           <div className="website"></div>
